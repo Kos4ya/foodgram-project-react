c7056078fc6d67e26f19d79db85a01fc204d911b
Kos4ya <palenovld@mail.ru>
2023-07-01 00:41:01 +0300

    new

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '130.193.34.207']
+ALLOWED_HOSTS = ['localhost', 'myhostsdfa.bounceme.net', '130.193.34.207']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
