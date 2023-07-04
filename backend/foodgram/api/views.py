2afdd6e519cf6a89fe99e7264f323f3532f0bac0
Kos4ya <palenovld@mail.ru>
2023-07-04 16:14:53 +0300

    new

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -81,8 +81,7 @@ class FavoriteView(APIView):
             )
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
